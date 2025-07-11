d9910472a9164314f3630be59a3f2ba9dfe9a232
Yashvir Gaming <64865096+YashvirGaming@users.noreply.github.com>
2025-07-11 19:04:33 +0400

    Delete Launch.bat

--- Launch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Launches OB2 EXE Compiler by Yashvir Gaming
-REM Make sure you have Python installed and required modules (tk, colorama, httpx, playsound, nuitka)
-
-python OB2_Compiler_GUI.py
-pause
